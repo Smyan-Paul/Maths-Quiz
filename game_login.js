4d57220341e5f2d0b0acb8cfe2ecf26173af9e6f
Smyan-Paul <87228812+Smyan-Paul@users.noreply.github.com>
2022-05-18 17:14:52 +0530

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -1,7 +1,7 @@
 add_user() 
 {
-   player1 = document.getElementById("player1_name_input").innerHTML;
-   player2 = document.getElementById("player2_name_input").innerHTML;
+   player1 = document.getElementById("player1_name_input").value;
+   player2 = document.getElementById("player2_name_input").value;
    localStorage.setItem("player1" , player1);
    localStorage.setItem("player2" , player2);
    
