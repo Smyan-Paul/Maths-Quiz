2493fd0e57424db17be09725c46f321bf340d16f
Smyan-Paul <87228812+Smyan-Paul@users.noreply.github.com>
2022-05-21 15:58:15 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,8 +1,8 @@
-add_user() 
+function add_user()
 {
-   player1 = document.getElementById("player1_name_input").value;
-   player2 = document.getElementById("player2_name_input").value;
+   player1 = document.getElementById("player1_name_input").innerHTML;
+   player2 = document.getElementById("player2_name_input").innerHTML;
    localStorage.setItem("player1" , player1);
    localStorage.setItem("player2" , player2);
-   
+   window.location = "quiz_game_page.html";
 }
